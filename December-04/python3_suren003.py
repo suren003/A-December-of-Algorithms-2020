05f3a7ba89b47d24baa4253de1f029b7b721199a
suren003 <56186001+suren003@users.noreply.github.com>
2020-12-04 07:33:09 +0530

    Added file

---
+++ December-04/python3_suren003.py
@@ -0,0 +1,67 @@
+def isleap(x):
+    if x%400 == 0:
+        return True
+    elif x%100 != 0 and x%4 == 0:
+        return True
+    else:
+        return False
+def daycount(x,y):
+    trty=[4,6,9,11]
+    if x==2:
+        if isleap(y):
+            return 29
+        else:
+            return 28
+    elif x in trty:
+        return 30
+    else:
+        return 31
+n=int(input("Number of Ice Creams : "))
+dates=input("Manufacture Dates : ")
+bbday=input("Best Befor days : ")
+givendate=input("Given date : ")
+bbday=bbday.split()
+mo=map(int,bbday)
+bbday=list(mo)
+i=0
+c=""
+count=0
+date=[]
+givendat=[]
+while i<len(dates):
+    if dates[i].isnumeric():
+        c+=(dates[i])
+        if not(dates[i+1].isnumeric()):
+            date.append(int(c))
+            c=""
+    i+=1
+i=0
+c=""
+while i<len(givendate):
+    if givendate[i].isnumeric():
+        c+=(givendate[i])
+        if not(givendate[i+1].isnumeric()):
+            givendat.append(int(c))
+            c=""
+    i+=1
+i=0
+while i<len(date)//3:
+    date[i*3]+=bbday[i]
+    while date[i*3]>daycount(date[i*3+1],date[i*3+2]):
+        date[i*3]-=daycount(date[i*3+1],date[i*3+2])
+        date[i*3+1]+=1
+        if date[i*3+1]>12:
+            date[i*3+1]-=12
+            date[i*3+2]+=1
+    if givendat[2]<date[i*3+2]:
+        count+=1
+    elif givendat[2]==date[i*3+2] and givendat[1]<date[i*3+1]:
+        count+=1
+    elif givendat[2]==date[i*3+2] and givendat[1]==date[i*3+1] and givendat[0]<=date[i*3]:
+        count+=1
+    else:
+        pass
+    i+=1
+print("No of ice creams spoiled:",n-count)
+
+
